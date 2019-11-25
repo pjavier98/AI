d3bde1525caf0dd6f1735122b04a18310f6a6efc
pjavier98 <pjpc@ic.ufal.br>
2019-11-25 11:04:33 -0300

    Minimax finalized

--- tic-tac-toe/board.py
+++ tic-tac-toe/board.py
@@ -1,392 +1,145 @@
-
-# struct Move 
-# { 
-# 	int row, col; 
-# }; 
-
-# char player = 'x', opponent = 'o'; 
-
-# // This function returns true if there are moves 
-# // remaining on the board. It returns false if 
-# // there are no moves left to play. 
-# bool isMovesLeft(char board[3][3]) 
-# { 
-# 	for (int i = 0; i<3; i++) 
-# 		for (int j = 0; j<3; j++) 
-# 			if (board[i][j]=='_') 
-# 				return true; 
-# 	return false; 
-# } 
-
-# // This is the evaluation function as discussed 
-# // in the previous article ( http://goo.gl/sJgv68 ) 
-# int evaluate(char b[3][3]) 
-# { 
-# 	// Checking for Rows for X or O victory. 
-# 	for (int row = 0; row<3; row++) 
-# 	{ 
-# 		if (b[row][0]==b[row][1] && 
-# 			b[row][1]==b[row][2]) 
-# 		{ 
-# 			if (b[row][0]==player) 
-# 				return +10; 
-# 			else if (b[row][0]==opponent) 
-# 				return -10; 
-# 		} 
-# 	} 
-
-# 	// Checking for Columns for X or O victory. 
-# 	for (int col = 0; col<3; col++) 
-# 	{ 
-# 		if (b[0][col]==b[1][col] && 
-# 			b[1][col]==b[2][col]) 
-# 		{ 
-# 			if (b[0][col]==player) 
-# 				return +10; 
-
-# 			else if (b[0][col]==opponent) 
-# 				return -10; 
-# 		} 
-# 	} 
-
-# 	// Checking for Diagonals for X or O victory. 
-# 	if (b[0][0]==b[1][1] && b[1][1]==b[2][2]) 
-# 	{ 
-# 		if (b[0][0]==player) 
-# 			return +10; 
-# 		else if (b[0][0]==opponent) 
-# 			return -10; 
-# 	} 
-
-# 	if (b[0][2]==b[1][1] && b[1][1]==b[2][0]) 
-# 	{ 
-# 		if (b[0][2]==player) 
-# 			return +10; 
-# 		else if (b[0][2]==opponent) 
-# 			return -10; 
-# 	} 
-
-# 	// Else if none of them have won then return 0 
-# 	return 0; 
-# } 
-
-# // This is the minimax function. It considers all 
-# // the possible ways the game can go and returns 
-# // the value of the board 
-# int minimax(char board[3][3], int depth, bool isMax) 
-# { 
-# 	int score = evaluate(board); 
-
-# 	// If Maximizer has won the game return his/her 
-# 	// evaluated score 
-# 	if (score == 10) 
-# 		return score; 
-
-# 	// If Minimizer has won the game return his/her 
-# 	// evaluated score 
-# 	if (score == -10) 
-# 		return score; 
-
-# 	// If there are no more moves and no winner then 
-# 	// it is a tie 
-# 	if (isMovesLeft(board)==false) 
-# 		return 0; 
-
-# 	// If this maximizer's move 
-# 	if (isMax) 
-# 	{ 
-# 		int best = -1000; 
-
-# 		// Traverse all cells 
-# 		for (int i = 0; i<3; i++) 
-# 		{ 
-# 			for (int j = 0; j<3; j++) 
-# 			{ 
-# 				// Check if cell is empty 
-# 				if (board[i][j]=='_') 
-# 				{ 
-# 					// Make the move 
-# 					board[i][j] = player; 
-
-# 					// Call minimax recursively and choose 
-# 					// the maximum value 
-# 					best = max( best, 
-# 						minimax(board, depth+1, !isMax) ); 
-
-# 					// Undo the move 
-# 					board[i][j] = '_'; 
-# 				} 
-# 			} 
-# 		} 
-# 		return best; 
-# 	} 
-
-# 	// If this minimizer's move 
-# 	else
-# 	{ 
-# 		int best = 1000; 
-
-# 		// Traverse all cells 
-# 		for (int i = 0; i<3; i++) 
-# 		{ 
-# 			for (int j = 0; j<3; j++) 
-# 			{ 
-# 				// Check if cell is empty 
-# 				if (board[i][j]=='_') 
-# 				{ 
-# 					// Make the move 
-# 					board[i][j] = opponent; 
-
-# 					// Call minimax recursively and choose 
-# 					// the minimum value 
-# 					best = min(best, 
-# 						minimax(board, depth+1, !isMax)); 
-
-# 					// Undo the move 
-# 					board[i][j] = '_'; 
-# 				} 
-# 			} 
-# 		} 
-# 		return best; 
-# 	} 
-# } 
-
-# // This will return the best possible move for the player 
-# Move findBestMove(char board[3][3]) 
-# { 
-# 	int bestVal = -1000; 
-# 	Move bestMove; 
-# 	bestMove.row = -1; 
-# 	bestMove.col = -1; 
-
-# 	// Traverse all cells, evaluate minimax function for 
-# 	// all empty cells. And return the cell with optimal 
-# 	// value. 
-# 	for (int i = 0; i<3; i++) 
-# 	{ 
-# 		for (int j = 0; j<3; j++) 
-# 		{ 
-# 			// Check if cell is empty 
-# 			if (board[i][j]=='_') 
-# 			{ 
-# 				// Make the move 
-# 				board[i][j] = player; 
-
-# 				// compute evaluation function for this 
-# 				// move. 
-# 				int moveVal = minimax(board, 0, false); 
-
-# 				// Undo the move 
-# 				board[i][j] = '_'; 
-
-# 				// If the value of the current move is 
-# 				// more than the best value, then update 
-# 				// best/ 
-# 				if (moveVal > bestVal) 
-# 				{ 
-# 					bestMove.row = i; 
-# 					bestMove.col = j; 
-# 					bestVal = moveVal; 
-# 				} 
-# 			} 
-# 		} 
-# 	} 
-
-# 	printf("The value of the best Move is : %d\n\n", 
-# 			bestVal); 
-
-# 	return bestMove; 
-# } 
-
-# // Driver code 
-
 class Board:
+	def __init__(self):
+		self.bestMoveRow = -1
+		self.bestMoveColumn = -1
+		self.player = 'x'
+		self.opponent = 'o'
+		self.depth = 0
+		self.board = [ 
+			[ '_', '_', '_' ], 
+			[ '_', 'o', '_' ], 
+			[ '_', '_', '_' ] 
+		]
 
-  def __init__(self):
-    self.player = 'x'
-    self.opponent = 'o';
-    self.board = [ 
-      [ '_', '_', '_' ], 
-      [ '_', '_', '_' ], 
-      [ '_', '_', '_' ] 
-    ]
+	def __str__(self):
+		table = self.board
+		return ('    0   1   2\n  #############\n0 #  {} | {} | {}\n  # -----------\n1 #  {} | {} | {}\n  # -----------\n2 #  {} | {} | {}\n\n\n'
+			.format(table[0][0], table[0][1], table[0][2], 
+							table[1][0], table[1][1], table[1][2],
+							table[2][0], table[2][1], table[2][2]))
 
-  def __str__(self):
-    table = self.board
-    return ('\t {} | {} | {}\n\t-----------\n\t {} | {} | {}\n\t-----------\n\t {} | {} | {}\n\n\n'
-      .format(table[0][0], table[0][1], table[0][2], 
-              table[1][0], table[1][1], table[1][2],
-              table[2][0], table[2][1], table[2][2]))
-  
-  def update_field(self, row, column, letter):
-    self.board[row][column] = letter
+	def update_field(self, row, column, isHuman):
+		if isHuman:
+			self.board[row][column] = self.player
+		else:
+			self.board[self.bestMoveRow][self.bestMoveColumn] = self.opponent
 
-  def isMovesLeft(self): 
-	  for i in range(3):
-		  for j in range(3):
-			  if (self.board[i][j]=='_'):
-				  return True
-	  return False 
+	def isMovesLeft(self): 
+		for i in range(3):
+			for j in range(3):
+				if (self.board[i][j]=='_'):
+					return True
+		return False 
 
-  def evaluate_function(self):
-    table = self.board
-	# Checking for Rows for X or O victory. 
-	for row in range(3):
-		if table[row][0]==table[row][1] and table[row][1] == table[row][2]: 
-			if table[row][0] == player:
-				return 10 
-			else if table[row][0] == opponent: 
-				return -10
-  
-  # Checking for Columns for X or O victory. 
-  for column in range(3):
-  		if table[0][column] == table[1][column] and table[1][column] == table[2][column]: 
-			if table[0][column] == player:
-				return 10 
-			else if table[0][column] == opponent: 
-				return -10
-
-
-	# Checking for Diagonals for X or O victory. 
-	if (table[0][0] == table[1][1] and table[1][1] == table[2][2]):
-		if (table[0][0] == player) 
-			return +10; 
-		else if (table[0][0] == opponent):
-			return -10; 
-
-	if (table[0][2] == table[1][1] and table[1][1] == table[2][0]): 
-		if (table[0][2] == player):
-			return +10
-		else if (table[0][2]==opponent):
-			return -10 
-
-	# Else if none of them have won then return 0 
-	return 0; 
-} 
-
-int minimax(char board[3][3], int depth, bool isMax) 
-{ 
-	int score = evaluate(board); 
-
-	// If Maximizer has won the game return his/her 
-	// evaluated score 
-	if (score == 10) 
-		return score; 
-
-# 	// If Minimizer has won the game return his/her 
-# 	// evaluated score 
-# 	if (score == -10) 
-# 		return score; 
-
-# 	// If there are no more moves and no winner then 
-# 	// it is a tie 
-# 	if (isMovesLeft(board)==false) 
-# 		return 0; 
-
-# 	// If this maximizer's move 
-# 	if (isMax) 
-# 	{ 
-# 		int best = -1000; 
-
-# 		// Traverse all cells 
-# 		for (int i = 0; i<3; i++) 
-# 		{ 
-# 			for (int j = 0; j<3; j++) 
-# 			{ 
-# 				// Check if cell is empty 
-# 				if (board[i][j]=='_') 
-# 				{ 
-# 					// Make the move 
-# 					board[i][j] = player; 
-
-# 					// Call minimax recursively and choose 
-# 					// the maximum value 
-# 					best = max( best, 
-# 						minimax(board, depth+1, !isMax) ); 
-
-# 					// Undo the move 
-# 					board[i][j] = '_'; 
-# 				} 
-# 			} 
-# 		} 
-# 		return best; 
-# 	} 
-
-# 	// If this minimizer's move 
-# 	else
-# 	{ 
-# 		int best = 1000; 
-
-# 		// Traverse all cells 
-# 		for (int i = 0; i<3; i++) 
-# 		{ 
-# 			for (int j = 0; j<3; j++) 
-# 			{ 
-# 				// Check if cell is empty 
-# 				if (board[i][j]=='_') 
-# 				{ 
-# 					// Make the move 
-# 					board[i][j] = opponent; 
-
-# 					// Call minimax recursively and choose 
-# 					// the minimum value 
-# 					best = min(best, 
-# 						minimax(board, depth+1, !isMax)); 
-
-# 					// Undo the move 
-# 					board[i][j] = '_'; 
-# 				} 
-# 			} 
-# 		} 
-# 		return best; 
-# 	} 
-# } 
+	def evaluate_function(self):
+		table = self.board
+		# Checking for Rows for X or O victory. 
+		for row in range(3):
+			if table[row][0]==table[row][1] and table[row][1] == table[row][2]: 
+				if table[row][0] == self.player:
+					return 10 
+				elif table[row][0] == self.opponent: 
+					return -10
+		
+		# Checking for Columns for X or O victory. 
+		for column in range(3):
+				if table[0][column] == table[1][column] and table[1][column] == table[2][column]: 
+					if table[0][column] == self.player:
+						return 10 
+					elif table[0][column] == self.opponent: 
+						return -10
 
 
+		# Checking for Diagonals for X or O victory. 
+		if table[0][0] == table[1][1] and table[1][1] == table[2][2]:
+			if table[0][0] == self.player:
+				return 10; 
+			elif table[0][0] == self.opponent:
+				return -10; 
 
+		if table[0][2] == table[1][1] and table[1][1] == table[2][0]: 
+			if table[0][2] == self.player:
+				return 10
+			elif table[0][2] == self.opponent:
+				return -10 
 
-# Move findBestMove(char board[3][3]) 
-# { 
-# 	int bestVal = -1000; 
-# 	Move bestMove; 
-# 	bestMove.row = -1; 
-# 	bestMove.col = -1; 
+		# Else if none of them have won then return 0 
+		return 0
 
-# 	// Traverse all cells, evaluate minimax function for 
-# 	// all empty cells. And return the cell with optimal 
-# 	// value. 
-# 	for (int i = 0; i<3; i++) 
-# 	{ 
-# 		for (int j = 0; j<3; j++) 
-# 		{ 
-# 			// Check if cell is empty 
-# 			if (board[i][j]=='_') 
-# 			{ 
-# 				// Make the move 
-# 				board[i][j] = player; 
+	def minimax(self, depth, isMax):
+		score = self.evaluate_function(); 
 
-# 				// compute evaluation function for this 
-# 				// move. 
-# 				int moveVal = minimax(board, 0, false); 
+		# If Maximizer has won the game return his/her 
+		if score == 10:
+			return score 
 
-# 				// Undo the move 
-# 				board[i][j] = '_'; 
+		# If Minimizer has won the game return his/her
+		if score == -10: 
+			return score
 
-# 				// If the value of the current move is 
-# 				// more than the best value, then update 
-# 				// best/ 
-# 				if (moveVal > bestVal) 
-# 				{ 
-# 					bestMove.row = i; 
-# 					bestMove.col = j; 
-# 					bestVal = moveVal; 
-# 				} 
-# 			} 
-# 		} 
-# 	} 
+		# If there are no more moves and no winner then 
+		if not self.isMovesLeft(): 
+			return 0
 
-# 	printf("The value of the best Move is : %d\n\n", 
-# 			bestVal); 
+		# If this maximizer's move 
+		if isMax:
+			best = -1000; 
+			
+			# Traverse all cells 
+			for i in range(0, 3):
+				for j in range(0, 3):
+					# Check if cell is empty 
+					if (self.board[i][j]=='_'):
+						# Make the move 
+						self.board[i][j] = self.player
 
-# 	return bestMove; 
-# } 
+						# Call minimax recursively and choose the maximum value 
+						best = max(best, self.minimax(depth + 1, not isMax))
+
+						# Undo the move 
+						self.board[i][j] = '_'
+			return best
+
+		# If this minimizer's move 
+		else:
+			best = 1000; 
+			
+			# Traverse all cells
+			for i in range(0, 3):
+				for j in range(0, 3):
+					# Check if cell is empty 
+					if (self.board[i][j]=='_'): 
+						# Make the move 
+						self.board[i][j] = self.opponent
+
+						# Call minimax recursively and choose the minimum value 
+						best = min(best, self.minimax(depth + 1, not isMax)) 
+
+						# Undo the move 
+						self.board[i][j] = '_'; 
+			return best
+
+	def findBestMove(self):
+		bestVal = -1000
+
+		# Traverse all cells, evaluate minimax function for 
+		# all empty cells. And return the cell with optimal value. 
+		for i in range(0, 3):
+				for j in range(0, 3):
+					# Check if cell is empty 
+					if self.board[i][j]=='_': 
+						# Make the move 
+						self.board[i][j] = self.player 
+
+						# Compute evaluation function for this move
+						moveVal = self.minimax(0, False) 
+
+						# Undo the move 
+						self.board[i][j] = '_' 
+
+						# If the value of the current move is 
+						# more than the best value, then update best
+						if moveVal > bestVal: 
+							self.bestMoveRow = i 
+							self.bestMoveColumn = j 
+							bestVal = moveVal
--- tic-tac-toe/tic-tac-toe.py
+++ tic-tac-toe/tic-tac-toe.py
@@ -10,26 +10,48 @@ def read_input(lower, upper):
       pass
     print("Invalid input, please choose again from [" + str(lower) + "-" + str(upper) + "]")
 
+# def valid_moviment(row, column):
 
-def game(turn):
+def game(board, isHuman):
   while True:
-    if (human):
-
+    score = board.evaluate_function()
+    if score == 10 and isHuman:
+        print('Human is the winner')
+        break
+    elif score == -10 and not isHuman:
+        print('AI is the winner')
+        break
+    
+    isHuman = not isHuman
+    if board.isMovesLeft():
+      if isHuman:
+        print('Human turns')
+        print('Enter the row and column: ', end='')
+        row, column = input().split()
+        row = int(row)
+        column = int(column)
+        # return
+        board.update_field(row, column, True)
+        
+      else:
+        print('AI turns')
+        # Find and update the best row and column based on the board
+        board.findBestMove()
+        board.update_field(-1, -1, False)
+      print('\n', str(board))
     else:
+      print('There was a draw')
+      break
 
 def main():
   print('###################################')
   print('# Welcome to the tic-tac-toe game #')
   print('###################################\n')
 
-  print('select [1] for Artificial Intelligence to start')
-  print('select [2] to you start')
-  choose = read_input(1, 2)
-
   board = Board()
-
-  # turn = 0
-  # choose == 1 ? turn 
-  game(1)
+  
+  # False -> IA start
+  print(str(board))
+  game(board, False)
 
 main()
